3a9297aff54a67e1071bf5e6ea21109e7acae3d4
nageewow <nd3@woolworths.com.au>
2020-09-22 17:46:23 +1000

    My first commit

---
+++ FirstVisualStudioProgram/FirstVisualStudioProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstVisualStudioProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
