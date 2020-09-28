40e969676e862289827b0850f8027c39b4710147
sandyatww <55424320+sandyatww@users.noreply.github.com>
2020-09-28 17:28:08 +1000

    refactored

---
+++ OrderMgmtSystem/OrderMgmtSystem.Test/UnitTest1.cs
@@ -0,0 +1,21 @@
+using System;
+using Xunit;
+
+namespace OrderMgmtSystem.Test
+{
+    public class UnitTest1
+    {
+
+        [Fact]
+        public void Test1()
+        {
+
+            // Create API client
+
+            // Call the API enpoint with correct payload, parameters etc
+
+            // assert the response. 
+
+        }
+    }
+}
---
+++ OrderMgmtSystem/OrderMgmtSystem/Services/OrderService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using OrderMgmtSystem.Entities;
+
+namespace OrderMgmtSystem.Services
+{
+    public class OrderService
+    {
+
+        CsvHelper csvHelper = new CsvHelper();
+
+        public OrderService()
+        {
+        }
+
+        internal void PlaceOrder(OrderDetails ordertdetials)
+        {
+            
+        }
+
+
+    }
+}
