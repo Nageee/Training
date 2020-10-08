733b185bfe32be076a7d465f91d2dbb62a01794a
nageewow <nd3@woolworths.com.au>
2020-10-08 16:52:17 +1100

    Write into CSV file

--- OrderMgmtSystem/OrderMgmtSystem/Helper/CsvHelper.cs
+++ OrderMgmtSystem/OrderMgmtSystem/Helper/CsvHelper.cs
@@ -17,18 +17,31 @@ namespace OrderMgmtSystem.Helper
 
             using (var reader = new StreamReader(Environment.CurrentDirectory + @"/Data/OrderInfoData.csv"))
             {
-              
+
                 while (!reader.EndOfStream)
                 {
                     var line = reader.ReadLine();
                     var values = line.Split(';');
 
-                   
+                    // write csv function
                 }
             }
 
 
+            // TextWriter sw = new StreamWriter(Environment.CurrentDirectory + @"/Data/OrderInfoData.csv"));
+            OrderDetails od = new OrderDetails();
 
+            string CustomerFirstName = od.CustomerFirstName;
+            string CustomerLastName = od.CustomerLastName;
+            using (TextWriter sw = new StreamWriter((Environment.CurrentDirectory + @"/Data/OrderInfoData.csv"))
+
+            {
+                string firstname = CustomerFirstName;
+            string lastname = CustomerLastName;
+
+            sw.WriteLine("{0},{1}", firstname, lastname);
+
+        }
 
             // Write Data
 
