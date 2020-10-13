f59fe52a5461adae359e051b539bfb8e3d20021f
Rnair1 <rnair1@woolworths.com.au>
2020-10-13 16:35:00 +1100

    added dropdown list

--- OrderMgmtSystem/OrderMgmtSystem/Entities/OrderDetails.cs
+++ OrderMgmtSystem/OrderMgmtSystem/Entities/OrderDetails.cs
@@ -3,15 +3,22 @@ namespace OrderMgmtSystem.Entities
 {
     public class OrderDetails
     {
-        private static object sync = new object();
-        private static int IdCount;
+        //private static object sync = new object();
+        //private static int IdCount;
         public string CustomerFirstName { get; set; }
         public string CustomerLastName { get; set; }
         public string AddressDetails { get; set; }
         public int PhoneNumber { get; set; }
         public string ItemNumber { get; set; }
-        public string ItemType { get; set; }
+        public Type ItemType { get; set; }
         public int OrderId { get; set; }
+
+        public enum Type
+        {
+            Chilled,
+            Frozen,
+            Ambient
+        }
 
        /* public OrderDetails(string _CustomerFirstName, string _CustomerLastName, string _AddressDetails, int _PhoneNumber, string _ItemNumber, string _ItemType)
         {
